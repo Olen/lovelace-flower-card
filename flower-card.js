50951da62fa844539b94975d18e11a7b9ac2a9af
Ola Thoresen <olen@inni.no>
2020-11-08 17:48:15 +0100

    Fix name and image attributes

--- flower-card.js
+++ flower-card.js
@@ -144,8 +144,8 @@ customElements.whenDefined('card-tools').then(() => {
       return cardTools.LitHtml`
       <ha-card>
         <div class="header" @click="${() => cardTools.moreInfo(this.stateObj.entity_id)}">
-          <img src="/local/images/plants/${this.config.species}.jpg">
-          <span id="name"> ${this.stateObj.attributes.friendly_name} </span>
+          <img src="${this.stateObj.attributes.image}.jpg">
+          <span id="name"> ${this.stateObj.attributes.name} </span>
           <span id="species">${species} </span>
         </div>
         <div class="divider"></div>
