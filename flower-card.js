0d95dec5c209d6223289e741e8d5848a863d2ab4
Ola Thoresen <ola@nytt.no>
2021-02-20 20:16:25 +0100

    Remove jpg exension

--- flower-card.js
+++ flower-card.js
@@ -144,7 +144,7 @@ customElements.whenDefined('card-tools').then(() => {
       return cardTools.LitHtml`
       <ha-card>
         <div class="header" @click="${() => cardTools.moreInfo(this.stateObj.entity_id)}">
-          <img src="${this.stateObj.attributes.image}.jpg">
+          <img src="${this.stateObj.attributes.image}">
           <span id="name"> ${this.stateObj.attributes.name} </span>
           <span id="species">${species} </span>
         </div>
